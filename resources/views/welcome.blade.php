9140faeaf1e193391231b944ae20afee585ac945
Steven Cook <steven@voltagead.com>
2015-12-08 18:14:30 -0700

    Changed splash page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5 Demo</div>
+				<div class="title">Laravel 5 Envoyer</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
